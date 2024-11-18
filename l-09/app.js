a5dd3d1f80ae677a3a0a807867d6acc5def3a487
KingShakeJS <kingshakejs@yandex.ru>
2024-11-18 22:28:01 +0300

    l-09

--- l-09/app.js
+++ l-09/app.js
@@ -1,9 +1,43 @@
-const http = require('http')
+const mysql = require('mysql')
 
-// 09 8:00
 
-http.createServer((req, res) => {
-    console.log('server work')
-    console.log(req)
-    res.end('end')
-}).listen(3003)
+
+const myConfig = {
+    host: 'localhost',
+    user: 'root',
+    database: 'my-site',
+    password: 'mysql'
+}
+
+const conn = mysql.createConnection({
+    host: myConfig.host,
+    user: myConfig.user,
+    database: myConfig.database,
+    password: myConfig.password
+})
+
+conn.connect(err => {
+    if (err) {
+        console.log(err)
+        return err
+    } else {
+        console.log('DataBase ------ OK')
+    }
+})
+
+let queryString = "SELECT * FROM users"
+conn.query(queryString, (err, result, field) => {
+    console.log(err)
+    console.log(result)
+    // console.log(result[1].username)
+    // console.log(field)
+})
+
+conn.end((err) => {
+    if (err) {
+        console.log(err)
+        return err
+    } else {
+        console.log('DataBase ------ Closed')
+    }
+})

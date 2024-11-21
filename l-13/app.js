4ce5a6730909ede9077f6a7ecb767b5aef5b880d
KingShakeJS <kingshakejs@yandex.ru>
2024-11-21 13:31:39 +0300

    l-13

---
+++ l-13/app.js
@@ -0,0 +1,13 @@
+const config = require('./config')
+const config2 = require('./config2')
+const fun =require('./fun')
+const f2 = require('./f2')
+
+
+// console.log(config)
+// console.log(config2)
+// console.log(fun(1,2))
+//
+// console.log(config2.f(4,3))
+
+console.log(f2(2))
